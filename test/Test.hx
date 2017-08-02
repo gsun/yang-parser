68a1562c7e613eeb5901fa25e5914d8943b1d019
gsun <sun_jungang@hotmail.com>
2017-08-02 15:36:55 +0800

    first draft

---
+++ test/Test.hx
@@ -0,0 +1,17 @@
+import Parser;
+import sys.io.File;
+
+class Test {
+	static function main(){
+		try {
+		    var result = Parser.parse(haxe.Resource.getString("yang"));
+		    sys.io.File.saveContent("result.yin",  haxe.Json.stringify(result));
+		} catch (e:Dynamic) {
+			if (e.location != null) {
+                trace(e.location.start.line + ":" + e.location.start.column + ": " + e.message);
+            } else {
+                trace(e.message);
+            }
+		}		
+	}
+}
