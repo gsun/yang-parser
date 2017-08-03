ff83ecfbbd92d68a2227986a61dd4ae5879ae04e
gsun <sun_jungang@hotmail.com>
2017-08-03 17:04:03 +0800

    minor fix

--- test/Test.hx
+++ test/Test.hx
@@ -4,7 +4,13 @@ import sys.io.File;
 class Test {
 	static function main(){
 		try {
-		    var result = Parser.parse(haxe.Resource.getString("yang"));
+			var resource = haxe.Resource.getString("yang");
+			
+			/*merge multi string into one in yang model before parse*/
+			var ereg:EReg = ~/["'][\s\r\n]*\+[\s\r\n]*["']/g;
+			resource = ereg.replace(resource, "");
+			
+		    var result = Parser.parse(resource);
 		    File.saveContent("result.json",  haxe.Json.stringify(result));
 		} catch (e:Dynamic) {
 			if (e.location != null) {
