45c1acdedfc8c51927c938395e873cb804655a0b
gsun <sun_jungang@hotmail.com>
2017-09-29 13:56:55 +0800

    convert tab to space

--- test/Test.hx
+++ test/Test.hx
@@ -3,39 +3,39 @@ import sys.io.File;
 import haxe.io.Path;
 
 class Test {
-	static function parseFile(infile:String) {
-		try {
+    static function parseFile(infile:String) {
+        try {
             var resource = File.getContent(infile);
-			
-			/*merge multi string into one in yang model before parse*/
-			var ereg:EReg = ~/["'][\s\r\n]*\+[\s\r\n]*["']/g;
-			resource = ereg.replace(resource, "");
-			
-		    var result = Parser.parse(resource);
-			var outfile = infile + ".json";
-		    File.saveContent(outfile,  haxe.Json.stringify(result));
-			//trace(infile + " parsed success");
-		} catch (e:Dynamic) {
-			trace(infile + " parsed fail");
-			if (e.location != null) {
+            
+            /*merge multi string into one in yang model before parse*/
+            var ereg:EReg = ~/["'][\s\r\n]*\+[\s\r\n]*["']/g;
+            resource = ereg.replace(resource, "");
+            
+            var result = Parser.parse(resource);
+            var outfile = infile + ".json";
+            File.saveContent(outfile,  haxe.Json.stringify(result));
+            //trace(infile + " parsed success");
+        } catch (e:Dynamic) {
+            trace(infile + " parsed fail");
+            if (e.location != null) {
                 trace(e.location.start.line + ":" + e.location.start.column + ": " + e.message);
             } else {
                 trace(e.message);
             }
-		}		
-	}
-	static function parsePath(path:String) {
-        if (sys.FileSystem.isDirectory(path)) {		
-			var entries = sys.FileSystem.readDirectory(path);
-			for (entry in entries) {
-			  parsePath(path + '/' + entry);
-			}
-		} else {
-			if (Path.extension(path) == "yang") parseFile(path);
-		}
-		
-	}
-	static function main(){
-		parsePath("./models");		
-	}
+        }       
+    }
+    static function parsePath(path:String) {
+        if (sys.FileSystem.isDirectory(path)) {     
+            var entries = sys.FileSystem.readDirectory(path);
+            for (entry in entries) {
+              parsePath(path + '/' + entry);
+            }
+        } else {
+            if (Path.extension(path) == "yang") parseFile(path);
+        }
+        
+    }
+    static function main(){
+        parsePath("./models");      
+    }
 }
