266848e15629922ce18b5ff841121ebd2820587e
Gang Sun <sun_jungang@hotmail.com>
2019-03-12 14:45:44 +0800

    revise

--- src/AstNameConflictVisitor.hx
+++ src/AstNameConflictVisitor.hx
@@ -4,74 +4,55 @@ using Lambda;
 class AstNameConflictVisitor extends AstVisitor {
         
     function container_stmt(stmt:Stmt, context:Dynamic) {      
-        var types = ["typedef_stmt", "grouping_stmt"]; 
-        for (i in types) {
-            nameConflict(stmt, i);
-            nameTopConflict(stmt, i);
-        }
+        check2(stmt, context);
     }
         
     function grouping_stmt(stmt:Stmt, context:Dynamic) {
-        var types = ["typedef_stmt", "grouping_stmt"]; 
-        for (i in types) {
-            nameConflict(stmt, i);
-            nameTopConflict(stmt, i);
-        }
+        check2(stmt, context);
     }
     
     function input_stmt(stmt:Stmt, context:Dynamic) {
-        var types = ["typedef_stmt", "grouping_stmt"]; 
-        for (i in types) {
-            nameConflict(stmt, i);
-            nameTopConflict(stmt, i);
-        }
+        check2(stmt, context);
     }
            
     function list_stmt(stmt:Stmt, context:Dynamic) {
-        var types = ["typedef_stmt", "grouping_stmt"]; 
-        for (i in types) {
-            nameConflict(stmt, i);
-            nameTopConflict(stmt, i);
-        }
+        check2(stmt, context);
     }
     
     function module_stmt(stmt:Stmt, context:Dynamic) { 
-        var types = ["typedef_stmt", "grouping_stmt", "feature_stmt", "identity_stmt", "extension_stmt"]; 
-        for (i in types) {
-            nameConflict(stmt, i);
-        }
+        check5(stmt, context);
     }
 
     function notification_stmt(stmt:Stmt, context:Dynamic) {
+        check2(stmt, context);
+    }
+        
+    function output_stmt(stmt:Stmt, context:Dynamic) {
+        check2(stmt, context);
+    }   
+                
+    function rpc_stmt(stmt:Stmt, context:Dynamic) {
+        check2(stmt, context);
+    }   
+    
+    function submodule_stmt(stmt:Stmt, context:Dynamic) {
+        check5(stmt, context);
+    }   
+    
+    function check2(stmt:Stmt, context:Dynamic) {
         var types = ["typedef_stmt", "grouping_stmt"]; 
         for (i in types) {
             nameConflict(stmt, i);
             nameTopConflict(stmt, i);
         }
     }
-        
-    function output_stmt(stmt:Stmt, context:Dynamic) {
-        var types = ["typedef_stmt", "grouping_stmt"]; 
-        for (i in types) {
-            nameConflict(stmt, i);
-            nameTopConflict(stmt, i);
-        }
-    }   
-                
-    function rpc_stmt(stmt:Stmt, context:Dynamic) {
-        var types = ["typedef_stmt", "grouping_stmt"]; 
-        for (i in types) {
-            nameConflict(stmt, i);
-            nameTopConflict(stmt, i);
-        }
-    }   
     
-    function submodule_stmt(stmt:Stmt, context:Dynamic) {
+    function check5(stmt:Stmt, context:Dynamic) {
         var types = ["typedef_stmt", "grouping_stmt", "feature_stmt", "identity_stmt", "extension_stmt"]; 
         for (i in types) {
             nameConflict(stmt, i);
         }
-    }   
+    }
             
     function nameConflict(stmt:Stmt, type:String) {
         var a = stmt.findSubs(type);
@@ -80,6 +61,7 @@ class AstNameConflictVisitor extends AstVisitor {
             assertTrue((count == 0), '${t.type} ${t.arg} name-conflict-error');
         }
     }
+    
     function nameTopConflict(stmt:Stmt, type:String) {
         var a = stmt.top.findSubs(type);
         var b = stmt.findSubs(type);
