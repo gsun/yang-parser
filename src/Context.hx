379c66eb209b2448d014bfa2a9b35919f9b762c7
Gang Sun <sun_jungang@hotmail.com>
2019-07-18 16:57:44 +0800

    revise

--- src/Context.hx
+++ src/Context.hx
@@ -2,9 +2,9 @@ import stmt.Stmt;
 
 class Context {
     public var raw:Map<String, StmtRaw>;
-    public var mo:Map<String, Stmt>;  //module name => Stmt
-    public var path:Map<String, String>; //module name => MO file path
-    public var yin:Bool;             //yang or yin
+    public var mo:Map<String, Stmt>;     //module tree: [module name => Stmt]
+    public var path:Map<String, String>; //module path map: [module name => MO file path]
+    public var yin:Bool;                 //yang or yin
     
     public function new() {
         raw = new Map();
