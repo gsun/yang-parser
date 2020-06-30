19608ed7b04a36b563fce0b643933213e2b321a7
Sun <gsun@coriant.com>
2020-06-30 11:23:27 +0800

    create instance with type

--- src/stmt/Stmt.hx
+++ src/stmt/Stmt.hx
@@ -121,22 +121,33 @@ class Stmt {
 
 	public var parent:Stmt;
 
-
-
 	public var status:StmtStatus;
 	public var ctx:Context;
 
 	static var validTypes:Array<String> = [
-		"anyxml_stmt", "argument_stmt", "augment_stmt", "base_stmt", "belongs_to_stmt", "bit_stmt", "case_stmt", "choice_stmt", "config_stmt", "contact_stmt",
-		"container_stmt", "default_stmt", "description_stmt", "enum_stmt", "error_app_tag_stmt", "error_message_stmt", "extension_stmt", "deviation_stmt",
-		"deviate_stmt", "feature_stmt", "fraction_digits_stmt", "grouping_stmt", "identity_stmt", "if_feature_stmt", "import_stmt", "include_stmt",
-		"input_stmt", "key_stmt", "leaf_stmt", "leaf_list_stmt", "length_stmt", "list_stmt", "mandatory_stmt", "max_elements_stmt", "min_elements_stmt",
-		"module_stmt", "must_stmt", "namespace_stmt", "notification_stmt", "ordered_by_stmt", "organization_stmt", "output_stmt", "path_stmt", "pattern_stmt",
-		"position_stmt", "prefix_stmt", "presence_stmt", "range_stmt", "reference_stmt", "refine_stmt", "require_instance_stmt", "revision_stmt",
-		"revision_date_stmt", "rpc_stmt", "status_stmt", "submodule_stmt", "type_stmt", "typedef_stmt", "unique_stmt", "units_stmt", "uses_stmt",
-		"value_stmt", "when_stmt", "yang_version_stmt", "yin_element_stmt", "unknown_stmt"
+		"anyxml_stmt", "argument_stmt", "augment_stmt", 
+		"base_stmt", "belongs_to_stmt", "bit_stmt", 
+		"case_stmt", "choice_stmt", "config_stmt", "contact_stmt","container_stmt", 
+		"default_stmt", "description_stmt", "deviation_stmt", "deviate_stmt", 
+		"enum_stmt", "error_app_tag_stmt", "error_message_stmt", "extension_stmt", 
+		"feature_stmt", "fraction_digits_stmt", 
+		"grouping_stmt", "identity_stmt", 
+		"if_feature_stmt", "import_stmt", "include_stmt", "input_stmt", 
+		"key_stmt", 
+		"leaf_stmt", "leaf_list_stmt", "length_stmt", "list_stmt", 
+		"mandatory_stmt", "max_elements_stmt", "min_elements_stmt", "module_stmt", "must_stmt", 
+		"namespace_stmt", "notification_stmt", 
+		"ordered_by_stmt", "organization_stmt", "output_stmt", 
+		"path_stmt", "pattern_stmt", "position_stmt", "prefix_stmt", "presence_stmt", 
+		"range_stmt", "reference_stmt", "refine_stmt", "require_instance_stmt", "revision_stmt", "revision_date_stmt", "rpc_stmt", 
+		"status_stmt", "submodule_stmt", 
+		"type_stmt", "typedef_stmt", 
+		"unique_stmt", "units_stmt", "uses_stmt", "unknown_stmt",
+		"value_stmt", 
+		"when_stmt", 
+		"yang_version_stmt", "yin_element_stmt"
 	];
-
+	
 	public var top(get, never):Stmt;
 
 	function get_top() {
@@ -208,16 +219,73 @@ class Stmt {
 	}
 
 	static public function buildStmt(raw:StmtRaw, ctx:Context):Stmt {
-		var stmt = switch raw.type {
-			case "base_stmt": new BaseStmt();
+		var stmt:Stmt = switch raw.type {
+			case "anyxml_stmt": new AnyxmlStmt(); 
+			case "argument_stmt": new ArgumentStmt();
+			case "augment_stmt": new AugmentStmt();
+			case "base_stmt": new BaseStmt(); 
 			case "belongs_to_stmt": new BelongsToStmt();
-			case "grouping_stmt": new GroupingStmt();
+			case "bit_stmt": new BitStmt();
+			case "case_stmt": new CaseStmt();
+			case "choice_stmt": new ChoiceStmt(); 
+			case "config_stmt": new ConfigStmt();
+			case "contact_stmt": new ContactStmt();
+			case "container_stmt": new ContainerStmt();
+			case "default_stmt": new DefaultStmt();
+			case "description_stmt": new DescriptionStmt();
+			case "deviation_stmt": new DeviationStmt();
+			case "deviate_stmt": new DeviateStmt(); 
+			case "enum_stmt": new EnumStmt();
+			case "error_app_tag_stmt": new ErrorAppTagStmt();
+			case "error_message_stmt": new ErrorMessageStmt();
+			case "extension_stmt": new ExtensionStmt();
+			case "feature_stmt": new FeatureStmt();
+			case "fraction_digits_stmt": new FractionDigitsStmt();
+			case "grouping_stmt": new GroupingStmt(); 
+			case "identity_stmt": new IdentityStmt();
 			case "if_feature_stmt": new IfFeatureStmt();
-			case "import_stmt": new ImportStmt();
+			case "import_stmt": new ImportStmt(); 
 			case "include_stmt": new IncludeStmt();
+			case "input_stmt": new InputStmt();
+			case "key_stmt": new KeyStmt();
+			case "leaf_stmt": new LeafStmt();
+			case "leaf_list_stmt": new LeafListStmt();
+			case "length_stmt": new LengthStmt();
+			case "list_stmt": new ListStmt();
+			case "mandatory_stmt": new MandatoryStmt(); 
+			case "max_elements_stmt": new MaxElementsStmt();
+			case "min_elements_stmt": new MinElementsStmt(); 
+			case "module_stmt": new ModuleStmt();
+			case "must_stmt": new MustStmt(); 
+			case "namespace_stmt": new NamespaceStmt();
+			case "notification_stmt": new NotificationStmt();
+			case "ordered_by_stmt": new OrderedByStmt();
+			case "organization_stmt": new OrganizationStmt();
+			case "output_stmt": new OutputStmt(); 
+			case "path_stmt": new PathStmt();
+			case "pattern_stmt": new PatternStmt(); 
+			case "position_stmt": new PositionStmt();
+			case "prefix_stmt": new PrefixStmt();
+			case "presence_stmt": new PresenceStmt();
+			case "range_stmt": new RangeStmt();
+			case "reference_stmt": new ReferenceStmt();
+			case "refine_stmt": new RefineStmt(); 
+			case "require_instance_stmt": new RequireInstanceStmt(); 
+			case "revision_stmt": new RevisionStmt();
+			case "revision_date_stmt": new RevisionDateStmt();
+			case "rpc_stmt": new RpcStmt();
+			case "status_stmt": new StatusStmt();
+			case "submodule_stmt": new SubmoduleStmt();
 			case "type_stmt": new TypeStmt();
+			case "typedef_stmt": new TypedefStmt();
+			case "unique_stmt": new UniqueStmt(); 
+			case "units_stmt": new UnitsStmt();
 			case "uses_stmt": new UsesStmt();
 			case "unknown_stmt": new UnknownStmt();
+			case "value_stmt": new ValueStmt();
+			case "when_stmt": new WhenStmt();
+			case "yang_version_stmt": new YangVersionStmt();
+			case "yin_element_stmt": new YinElementStmt();
 			default: new Stmt();
 		}
 		stmt.raw = raw;
